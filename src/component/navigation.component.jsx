5fb0d246c38800f467d189cb9ddfcb7c0d086d16
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-08-28 22:00:41 +0100

    Done with the nav bar

--- src/component/navigation.component.jsx
+++ src/component/navigation.component.jsx
@@ -1,22 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
+import {ReactComponent as CodeXLogoSVG} from "../assets/codex-logo.svg"
 
 const NavigationComponent = () => {
     return <>
-        <div className="container bg-primaryWhite shadow-md">
-        <div className="flex row justify-between items-center">
-            <div className="logo">
-
+        <nav className="relative container bg-primaryWhite shadow-md mx-auto px-[4%] py-4">
+            <div className="flex row justify-between items-center">
+                <div className="logo">
+                    <CodeXLogoSVG />
+                </div>
+                <div>
+                    <ul>
+                        <NavLink>Home</NavLink>
+                        <NavLink>About Us</NavLink>
+                        <NavLink>Blog</NavLink>
+                    </ul>
+                </div>
+                <Link className="rounded-lg text-white bg-primaryBlack px-5 py-2">Join Codex</Link>
             </div>
-            <div>
-                <ul>
-                    <NavLink>Home</NavLink>
-                    <NavLink>About Us</NavLink>
-                    <NavLink>Blog</NavLink>
-                </ul>
-            </div>
-            <Link className="rounded-lg text-white">Join Codex</Link>
-        </div>
-    </div>
+        </nav>
     </>
 }
 
